aba910b3b35113747323b84139a523d7e87dd9b4
leahdonuts <lrdomas@gmail.com>
2024-04-08 22:55:46 +0100

    Add Review Form

---
+++ frontend/src/Components/ReviewForm/ReviewForm.test.tsx
@@ -0,0 +1,53 @@
+import { describe, test } from "@jest/globals";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import ReviewForm from "./ReviewForm";
+import {
+  mockMoviesData,
+  mockPostReviewRequest,
+  mockPostReviewResponse,
+} from "../../testData";
+import { postData } from "../../api";
+
+jest.mock("../../api");
+const mockPostData = postData as jest.Mock;
+
+const component = (
+  <ReviewForm
+    selectedMovie={mockMoviesData[0]}
+    setSelectedMovie={jest.fn()}
+    handleFormSubmission={jest.fn()}
+  />
+);
+
+describe("ReviewForm", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Should match default snapshot", async () => {
+    const { asFragment } = render(component);
+
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  test("Should submit form with correct review", async () => {
+    mockPostData.mockReturnValueOnce(Promise.resolve(mockPostReviewResponse));
+
+    const { getByTestId, getByLabelText } = render(component);
+
+    const reviewInput = getByLabelText("Movie Review");
+    const submitButton = getByTestId("submit-button");
+
+    fireEvent.change(reviewInput, {
+      target: { value: mockPostReviewRequest.review },
+    });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() =>
+      expect(mockPostData).toHaveBeenCalledWith("submitReview", {
+        review: mockPostReviewRequest.review,
+      })
+    );
+  });
+});
---
+++ frontend/src/Components/ReviewForm/ReviewForm.tsx
@@ -0,0 +1,122 @@
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { usePostReview } from "../../hooks/usePostReview/usePostReview";
+import { useState } from "react";
+import { Movie } from "../../types";
+
+interface Props {
+  selectedMovie: Movie;
+  setSelectedMovie: React.Dispatch<React.SetStateAction<Movie | undefined>>;
+  handleFormSubmission: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const classes = {
+  form: "review-form",
+};
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  padding: 20,
+
+  [`& .${classes.form}`]: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    gap: 24,
+
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+  },
+}));
+
+const ReviewForm = ({
+  selectedMovie,
+  setSelectedMovie,
+  handleFormSubmission,
+}: Props) => {
+  const [reviewResponse, setReviewResponse] = useState<string>();
+
+  // Post Hook
+  const {
+    isLoading: isSubmitReviewLoading,
+    error: submitReviewError,
+    postReview,
+  } = usePostReview();
+
+  const validationSchema = yup.object({
+    review: yup
+      .string()
+      .required("Review is required to submit")
+      .max(100, "Review must be less than 100 characters"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      review: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (value) => {
+      const response = await postReview(value.review);
+
+      if ((!isSubmitReviewLoading || !submitReviewError) && response) {
+        setReviewResponse(response.message);
+        handleFormSubmission(response.message);
+      }
+
+      setTimeout(() => {
+        setSelectedMovie(undefined);
+        setReviewResponse(undefined);
+        handleFormSubmission("");
+      }, 2000);
+    },
+  });
+
+  return (
+    <StyledContainer data-testid="review-form">
+      <Typography style={{ marginBottom: "4px" }}>
+        Please Enter Review for{" "}
+        <span style={{ fontWeight: "bold", fontStyle: "italic" }}>
+          {selectedMovie.title}
+        </span>
+      </Typography>
+      <form onSubmit={formik.handleSubmit} className={classes.form}>
+        <TextField
+          fullWidth
+          variant="standard"
+          id="review"
+          name="review"
+          label="Movie Review"
+          value={formik.values.review}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.review && Boolean(formik.errors.review)}
+          helperText={formik.touched.review && formik.errors.review}
+          disabled={!!reviewResponse}
+        />
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          data-testid="submit-button"
+          disabled={!!reviewResponse || !formik.isValid}
+        >
+          Submit
+        </Button>
+      </form>
+    </StyledContainer>
+  );
+};
+
+export default ReviewForm;
